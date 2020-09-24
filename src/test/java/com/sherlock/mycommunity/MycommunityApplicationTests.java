a1d090e03d53e90c4811766d9bb6385f32b34e89
Sherlock <530317349@qq.com>
2020-09-24 19:19:47 +0800

    init repo

---
+++ src/test/java/com/sherlock/mycommunity/MycommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sherlock.mycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
